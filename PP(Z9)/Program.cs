7c623e6aff6292cc2d22618d4bfae5f74a4478d8
alina.timonina172372 <alina.timonina172372@gmail.com>
2020-05-20 17:33:39 +0300

    Добавьте файлы проекта.

---
+++ PP(Z9)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер последовательности: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Заданнное число: ");
+            int k = Int32.Parse(Console.ReadLine());
+            double[] arr = new double[n];
+            FileStream f = new FileStream("C:/Users/Алина/Desktop/t.dat", FileMode.Open);
+            BinaryWriter fOut = new BinaryWriter(f);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("arr[{0}] = ", i);
+                arr[i] = double.Parse(Console.ReadLine());
+                fOut.Write(arr[i]);
+            }
+            fOut.Close();
+            Console.WriteLine("\n"+"Результат: ");
+            f = new FileStream("C:/Users/Алина/Desktop/t.dat", FileMode.Open);
+            BinaryReader fIn = new BinaryReader(f);
+            long m = f.Length;
+            for (long i = 8; i < m; i += 16)
+            { 
+                f.Seek(i, SeekOrigin.Begin);
+                arr[0] = fIn.ReadDouble();
+                if (arr[0] > k)
+                {
+                    Console.WriteLine("{0:f2}", arr[0]);
+                }
+            }
+            fIn.Close();
+            f.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PP(Z9_1)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Z9_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("C:/Users/Алина/Desktop/text.txt");
+            int n = 1;
+            while (n < lines.Length)
+            {
+                Console.WriteLine(lines[n]);
+                n += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
